026c45ee4e9f3e0591481fff5b23460143ab7dfe
noahdrakes <noah.i.drakes@gmail.com>
2024-08-21 13:07:20 -0400

    added my plotting scripts and some sample data

---
+++ plotting/EncoderVelocityBFT.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load the first CSV file
+df1 = pd.read_csv('basic_force_test_no_obstacle.csv')
+
+# Load the second CSV file
+df2 = pd.read_csv('basic_force_test_with_obstacle.csv')
+
+# Plot the first data set
+plt.plot(df1['TIMESTAMP'], df1['ENCODER_VEL_2'], label='Encoder Count with no obstacle')
+
+# Plot the second data set
+plt.plot(df2['TIMESTAMP'], df2['ENCODER_VEL_2'], label='Encoder Count with obstacle')
+
+# Adding titles and labels
+plt.title('Encoder Velocity with obstacle vs no obstacle')
+plt.xlabel('Timestamp (s)')
+plt.ylabel('Encoder Velocity')
+
+# Show the legend to differentiate the lines
+plt.legend()
+
+# Display the plot
+plt.show()
---
+++ plotting/motorCurrentBFT.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load the first CSV file
+df1 = pd.read_csv('basic_force_test_no_obstacle.csv')
+
+# Load the second CSV file
+df2 = pd.read_csv('basic_force_test_with_obstacle.csv')
+
+# Plot the first data set
+plt.plot(df1['TIMESTAMP'], df1['MOTOR_CURRENT_2'], label='Motor Current with no obstacle')
+
+# Plot the second data set
+plt.plot(df2['TIMESTAMP'], df2['MOTOR_CURRENT_2'], label='Motor Current with obstacle')
+
+# Adding titles and labels
+plt.title('Motor Current with obstacle vs no obstacle')
+plt.xlabel('Timestamp (s)')
+plt.ylabel('Motor Current')
+
+# Show the legend to differentiate the lines
+plt.legend()
+
+# Display the plot
+plt.show()
